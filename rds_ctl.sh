355f1c1874d5bbb40584b7359e8d4e94e0554d84
TheME <andreas_fl@outlook.com>
2017-08-26 10:44:50 +0200

    Update rds_ctl.sh

--- rds_ctl.sh
+++ rds_ctl.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
- echo > /home/pi/rds_ctl
+ echo >rds_ctl
 
 while pgrep -fl "pi_fm_rds" >/dev/null;
 do
 
-title=$(awk 'NR==1; END{print}' /home/pi/temp.txt | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="StreamTitle"{print $2}' | cut -c 2- | sed 's/..$//')
+title=$(awk 'NR==1; END{print}' temp.txt | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="StreamTitle"{print $2}' | cut -c 2- | sed 's/..$//')
 
 
 if [ "$title" != "$title_old" ]; then
 
    title_old=$title
-   echo RT $title >>/home/pi/rds_ctl
+   echo RT $title >>rds_ctl
 fi
    sleep 1;
 done
