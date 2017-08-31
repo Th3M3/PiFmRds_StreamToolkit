358f9226b560da306fc443166f82df72c70f589d
TheME <andreas_fl@outlook.com>
2017-08-31 14:48:30 +0200

    Update rds_ctl.sh

--- rds_ctl.sh
+++ rds_ctl.sh
@@ -5,13 +5,32 @@
 while pgrep -fl "pi_fm_rds" >/dev/null;
 do
 
-title=$(awk 'NR==1; END{print}' temp.txt | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="StreamTitle"{print $2}' | cut -c 2- | sed 's/..$//')
+# read from temp.txt and parse meta title
+title=$(tail -1 temp.txt | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="StreamTitle"{print $2}' | cut -c 2- | sed 's/..$//')
 
-
+# check if title has changed
 if [ "$title" != "$title_old" ]; then
-   if [ -z "$title_old" ]; then sleep 5; else sleep 12; fi
+
+   # if first run, wait 3 sec before output Radio Text
+   if [ -z "$title_old" ]; then sleep 3; fi
+
+   # replace characters to RDS-Charset
+   title=$(echo $title | sed 's/[ÀÂ]/A/g' | sed 's/[àâ]/a/g')
+   title=$(echo $title | sed 's/[Ä]/Ae/g' | sed 's/[ä]/ae/g')
+   title=$(echo $title | sed 's/[ÉÈÊË]/E/g' | sed 's/[éèêë]/e/g')
+   title=$(echo $title | sed 's/[ÎÏ]/I/g' | sed 's/[îï]/i/g')
+   title=$(echo $title | sed 's/[Ö]/Oe/g' | sed 's/[ö]/oe/g')
+   title=$(echo $title | sed 's/[Ü]/Ue/g' | sed 's/[ü]/ue/g')
+
+   # write radio text (or clear if is has been removed)
+   if [ -z "$title" ]; then
+      echo "RT  " >>rds_ctl
+   else
+      echo RT "${title}" >>rds_ctl
+   fi
+
    title_old=$title
-   echo RT $title >>rds_ctl
+
 fi
    sleep 1;
 done
