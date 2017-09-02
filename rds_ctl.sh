c93999692f9f14e1aa3bf2c6bfca4453452014fa
TheME <andreas_fl@outlook.com>
2017-09-02 15:08:22 +0200

    Update rds_ctl.sh

--- rds_ctl.sh
+++ rds_ctl.sh
@@ -19,16 +19,17 @@ EscapeChars() {
    # special characters
    ret=$(echo $ret | sed 's/[|]/-/g')
    ret=$(echo $ret | sed 's/[&]/+/g')
+   ret=$(echo $ret | sed 's/[§$%]//g')
 
-   # shorten string if it is too long (short strings)
-   if [ "${#ret}" -gt 50 ];
+   # try to shorten string if it is too long (short strings)
+   if [ ${#ret} -gt 50 ];
    then
-      ret=$(echo $ret | sed 's/ feat. /, /g')
+      ret=$(echo $ret | sed 's/ feat. /; /g')
       ret=$(echo $ret | sed 's/ featuring / feat. /g' | sed 's/Featuring /Feat. /g')
    fi
 
-   # shorten more if it is still too long (remove strings in Brackets)
-   if [ "${#ret}" -gt 50 ];
+   # try to shorten more if it is still too long (remove strings in Brackets)
+   if [ ${#ret} -gt 50 ];
    then
       ret=$(echo $ret | sed 's/ [[A-Z a-z]*]//g')
       ret=$(echo $ret | sed 's/ ([A-Z a-z]*)//g')
@@ -42,29 +43,26 @@ do
    # catch Stream's Station Name
    if [ "$(echo "$line" | cut -c 1-9)" = 'ICY-NAME:' ];
    then
-      EscapeChars "$(echo $line | cut -c11-)"
-      station=$ret
+      station=$(echo $line | cut -c11-)
    fi
 
    # catch Stream's Current Title
    if [ "$(echo "$line" | cut -c 1-9)" = 'ICY-META:' ];
    then
       # parse meta data from line
-      title=$(echo $line | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="StreamTitle"{print $2}' | cut -c 2- | sed 's/..$//')
-
-      EscapeChars "$title"
-
+      title=$(echo "$line" | sed -r 's/[[:alnum:]]+=/\n&/g' | awk -F= '$1=="StreamTitle"{print $2}' | cut -c 2- | sed 's/..$//')
       # use Station Name if there is currently no Title Info
-      if [ -z "$title" -a "$title"!=" " ];
+      if [ -z "$title" ];
       then
-         title=$station
+         title="$station"
       fi
 
       # write Title Info if it has changed
       if [ "$title" != "$title_old" ];
       then
-         echo RT "${ret}" >>rds_ctl
-         title_old=$title
+         EscapeChars "$title"
+         echo RT "$ret" >>rds_ctl
+         title_old="$title"
       fi
    fi
 done < DataPipe
